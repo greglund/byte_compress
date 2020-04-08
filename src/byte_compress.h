357874dfe51d2094e2c5773b72cd6b1c3a1c85a6
Greg Lund <greg@4iiii.com>
2020-04-07 22:07:17 -0600

    Add doxyfile.  Remove dependency on stdint.  Formatting.

--- src/byte_compress.h
+++ src/byte_compress.h
@@ -1,9 +1,7 @@
+/// @file
 
 #if !defined(BYTE_COMPRESS_H_)
 #define BYTE_COMPRESS_H_
-
-#include <stdint.h>
-
 
 #if defined(__cplusplus)
 extern "C"
@@ -11,24 +9,29 @@ extern "C"
 #endif
 
 /// Compress an array of bytes.
+///
 /// Assumptions:
+///
 /// 1) incoming bytes will only have values between 0x0 and 0x7F
+///
 /// 2) byte values are likely repeated sequencially
 ///
 /// Simple compression scheme:
+///
 /// A single byte will be placed into the output, unencoded. 
+///
 /// Multiple bytes: Encode byte count ORed with 0x80, followed by 
 /// the byte value
 ///
 /// Note that a byte value exceeding 0x7F in the input stream will
 /// cause the function to return prematurely.
 /// 
-/// @param data_ptr IN Pointer to data to be compressed
-///                 OUT Pointer to compressed data
-/// @param data_size Size of the input data
+/// @param[in] data_ptr Pointer to data to be compressed
+/// @param[out] data_ptr Pointer to compressed data
+/// @param[in] data_size Size of the input data
 /// @return Compressed data size if successful.  Otherwise, the
 /// negative (offset + 1) at which an error occurred.
-int byte_compress(uint8_t* data_ptr, int data_size);
+int byte_compress(char* data_ptr, int data_size);
 
 #if defined(__cplusplus)
 }
--- src/byte_decompress.c
+++ src/byte_decompress.c
@@ -1,30 +1,21 @@
 
 #include "byte_decompress.h"
 
-#include <stdint.h>
 
-
-int byte_decompress(const uint8_t* data_ptr, int data_size, uint8_t* out, int output_size_max)
+int byte_decompress(const char* data_ptr, int data_size, char* out, int output_size_max)
 {
-   int size = 0;
-   int count = 0;
-   int i;
+   int size = 0, count = 0, i;
 
-   while (data_size--)
-   {
-      if ((*data_ptr & 0x80) && (data_size != 0))
-      {
-         count = *data_ptr & 0x7F;
+   while (data_size--) {
+      if ((*data_ptr & 0x80) && (data_size != 0)) {
+         count = (unsigned char) *data_ptr & 0x7F;
          data_ptr++;
          data_size--;
-      }
-      else
-      {
+      } else {
          count = 1;
       }
 
-      for (i = 0; i < count; i++)
-      {
+      for (i = 0; i < count; i++) {
          if (size < output_size_max)
             *out++ = *data_ptr;
 
