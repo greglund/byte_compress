579c9fa1dc412b375e1a80e650ac0de21d799003
Greg Lund <greg.lund@gmail.com>
2020-04-07 22:07:17 -0600

    Add doxyfile.  Remove dependency on stdint.  Formatting.

--- src/byte_compress.c
+++ src/byte_compress.c
@@ -1,26 +1,22 @@
 
 #include "byte_compress.h"
 
-#include <stdint.h>
-
 
 #define BYTE_COMPRESS_OUT \
    if (count > 1) \
    { \
-      *out++ = 0x80 | count; \
+      *out++ = 0x80 | (unsigned char) count; \
       size++; \
    } \
    \
    *out++ = val; \
    size++;
 
-int byte_compress(uint8_t* data_ptr, int data_size)
+int byte_compress(char* data_ptr, int data_size)
 {
-   uint8_t* out;
-   int index;
-   int size;
-   int count;
-   uint8_t val;
+   char* out;
+   int index, size, count;
+   char val;
 
    if (data_size == 0)
       return 0;
@@ -31,20 +27,16 @@ int byte_compress(uint8_t* data_ptr, int data_size)
    out = data_ptr;
    val = *data_ptr++;
    size = 0;
-   index = 1;
-   count = 1;
+   index = count = 1;
 
-   while (--data_size)
-   {
-      if (*data_ptr & 0x80)
-      {
+   while (--data_size) {
+      if (*data_ptr & 0x80) {
          BYTE_COMPRESS_OUT
          return -(index + 1); // Invalid value in nth item
       }
 
       if ((*data_ptr != val) ||
-            (count == 0x7F)) // Ensure the count does not overflow
-      {
+            (count == 0x7F)) { // Ensure the count does not overflow
          // Output the count and value, begin counting again.
          BYTE_COMPRESS_OUT
          count = 0;
--- test/test/test_bytecompress.c
+++ test/test/test_bytecompress.c
@@ -1,5 +1,4 @@
 
-#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -13,7 +12,7 @@
  
 
 // Sample data for compression
-static const uint8_t test_data_ptr[] =
+static const char test_data_ptr[] =
 { 
    0x03, 0x74, 0x04, 0x04, 0x04, 0x35, 0x35, 0x64,
    0x64, 0x64, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -21,23 +20,23 @@ static const uint8_t test_data_ptr[] =
 };
 
 // Expected output after encoding
-static const uint8_t test_data_expected[] = {0x03, 0x74, 0x83, 0x04, 0x82, 0x35, 0x84, 0x64, 0x85, 0x00, 0x56, 0x45, 0x83, 0x56, 0x83, 0x09};
+static const char test_data_expected[] = {0x03, 0x74, 0x83, 0x04, 0x82, 0x35, 0x84, 0x64, 0x85, 0x00, 0x56, 0x45, 0x83, 0x56, 0x83, 0x09};
 
 // Single item
-static const uint8_t test_data_single[] = {0x03};
+static const char test_data_single[] = {0x03};
 
 // Data where subsequent bytes are not equal; therefore
 // no compression is expected and it should remain
 // unchanged in the output
-static const uint8_t test_data_no_compression[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+static const char test_data_no_compression[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 // Upon compressing an input array with 128 of the same value "0x35" 
 // we expect the count be encoded at its maximum and then continue
 // encoding normally again.
-static const uint8_t test_data_overflow_count_expected[] = {0xFF, 0x35, 0x35};
+static const char test_data_overflow_count_expected[] = {0xFF, 0x35, 0x35};
 
 // Data with an invalid byte in the first item.
-static const uint8_t test_data_invalid1[] =
+static const char test_data_invalid1[] =
 { 
    0x83, 0x74, 0x04, 0x04, 0x04, 0x35, 0x35, 0x64,
    0x64, 0x64, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -45,7 +44,7 @@ static const uint8_t test_data_invalid1[] =
 };
 
 // Data with an invalid byte in the 24th item.
-static const uint8_t test_data_invalid2[] =
+static const char test_data_invalid2[] =
 { 
    0x03, 0x74, 0x04, 0x04, 0x04, 0x35, 0x35, 0x64,
    0x64, 0x64, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -56,13 +55,13 @@ static const uint8_t test_data_invalid2[] =
 void test_byte_compress(void)
 {
    int new_size;
-   uint8_t data_ptr[sizeof(test_data_ptr)];
+   char data_ptr[sizeof(test_data_ptr)];
 
    memcpy(data_ptr, test_data_ptr, sizeof(data_ptr));
    new_size = byte_compress(data_ptr, sizeof(data_ptr));
    printf("New size: %i\r\n", new_size);
    TEST_ASSERT_EQUAL((int) sizeof(test_data_expected), new_size);
-   TEST_ASSERT_EQUAL_UINT8_ARRAY(test_data_expected, data_ptr, sizeof(test_data_expected));
+   TEST_ASSERT_EQUAL_CHAR_ARRAY(test_data_expected, data_ptr, sizeof(test_data_expected));
 }
 
 void test_byte_compress_no_data(void)
@@ -76,40 +75,40 @@ void test_byte_compress_no_data(void)
 void test_byte_compress_single(void)
 {
    int new_size;
-   uint8_t data_ptr[sizeof(test_data_single)];
+   char data_ptr[sizeof(test_data_single)];
 
    memcpy(data_ptr, test_data_single, sizeof(data_ptr));
    new_size = byte_compress(data_ptr, sizeof(data_ptr));
    TEST_ASSERT_EQUAL((int) sizeof(test_data_single), new_size);
-   TEST_ASSERT_EQUAL_UINT8_ARRAY(test_data_single, data_ptr, sizeof(test_data_single));
+   TEST_ASSERT_EQUAL_CHAR_ARRAY(test_data_single, data_ptr, sizeof(test_data_single));
 }
 
 void test_byte_compress_no_compression(void)
 {
    int new_size;
-   uint8_t data_ptr[sizeof(test_data_no_compression)];
+   char data_ptr[sizeof(test_data_no_compression)];
 
    memcpy(data_ptr, test_data_no_compression, sizeof(data_ptr));
    new_size = byte_compress(data_ptr, sizeof(data_ptr));
    TEST_ASSERT_EQUAL((int) sizeof(test_data_no_compression), new_size);
-   TEST_ASSERT_EQUAL_UINT8_ARRAY(test_data_no_compression, data_ptr, sizeof(test_data_no_compression));
+   TEST_ASSERT_EQUAL_CHAR_ARRAY(test_data_no_compression, data_ptr, sizeof(test_data_no_compression));
 }
 
 void test_byte_compress_count_overflow(void)
 {
    int new_size;
-   uint8_t data_ptr[128];
+   char data_ptr[128];
 
    memset(data_ptr, 0x35, sizeof(data_ptr));
    new_size = byte_compress(data_ptr, sizeof(data_ptr));
    TEST_ASSERT_EQUAL((int) sizeof(test_data_overflow_count_expected), new_size);
-   TEST_ASSERT_EQUAL_UINT8_ARRAY(test_data_overflow_count_expected, data_ptr, sizeof(test_data_overflow_count_expected));
+   TEST_ASSERT_EQUAL_CHAR_ARRAY(test_data_overflow_count_expected, data_ptr, sizeof(test_data_overflow_count_expected));
 }
 
 void test_byte_compress_invalid1(void)
 {
    int new_size;
-   uint8_t data_ptr[sizeof(test_data_invalid1)];
+   char data_ptr[sizeof(test_data_invalid1)];
 
    memcpy(data_ptr, test_data_invalid1, sizeof(data_ptr));
    new_size = byte_compress(data_ptr, sizeof(data_ptr));
@@ -119,7 +118,7 @@ void test_byte_compress_invalid1(void)
 void test_byte_compress_invalid2(void)
 {
    int new_size;
-   uint8_t data_ptr[sizeof(test_data_invalid2)];
+   char data_ptr[sizeof(test_data_invalid2)];
 
    memcpy(data_ptr, test_data_invalid2, sizeof(data_ptr));
    new_size = byte_compress(data_ptr, sizeof(data_ptr));
@@ -129,23 +128,23 @@ void test_byte_compress_invalid2(void)
 void test_byte_decompress(void)
 {
    int new_size;
-   uint8_t data_out[sizeof(test_data_ptr)];
+   char data_out[sizeof(test_data_ptr)];
 
    new_size = byte_decompress(test_data_expected, sizeof(test_data_expected), data_out, sizeof(data_out));
    TEST_ASSERT_EQUAL((int) sizeof(test_data_ptr), new_size);
-   TEST_ASSERT_EQUAL_UINT8_ARRAY(test_data_ptr, data_out, sizeof(test_data_ptr));
+   TEST_ASSERT_EQUAL_CHAR_ARRAY(test_data_ptr, data_out, sizeof(test_data_ptr));
 }
 
 void test_byte_decompress_count_max(void)
 {
    int new_size;
-   uint8_t data_expected[128];
-   uint8_t data_out[128];
+   char data_expected[128];
+   char data_out[128];
 
    memset(data_expected, 0x35, sizeof(data_expected));
    memset(data_out, 0, sizeof(data_out));
    new_size = byte_decompress(test_data_overflow_count_expected, sizeof(test_data_overflow_count_expected), data_out, sizeof(data_out));
    TEST_ASSERT_EQUAL((int) sizeof(data_expected), new_size);
-   TEST_ASSERT_EQUAL_UINT8_ARRAY(data_expected, data_out, sizeof(data_expected));
+   TEST_ASSERT_EQUAL_CHAR_ARRAY(data_expected, data_out, sizeof(data_expected));
 }
 
